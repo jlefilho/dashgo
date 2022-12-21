aba755cf3d33c0b5be826a822d44d29af61ff048
Jorge Espindola <jlespindolaf@gmail.com>
2022-12-21 10:17:52 -0300

    Refactoring components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,26 @@
+import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+
+export function Profile() {
+    return (
+        <Flex align='center'>
+            <Box
+                textAlign='right'
+                mr='4'
+            >
+                <Text>Jorge Espindola</Text>
+                <Text
+                    color='gray.300'
+                    fontSize='small'
+                >
+                    jlespindolaf@gmail.com
+                </Text>
+            </Box>
+
+            <Avatar
+                size='md'
+                name='Jorge Espindola'
+                src='https://github.com/jlefilho.png'
+            />
+        </Flex>
+    )
+}
---
+++ src/components/Header/SearchBox.tsx
@@ -0,0 +1,32 @@
+import { Flex, Icon, Input } from "@chakra-ui/react";
+import { RiSearchLine } from "react-icons/ri";
+
+export function SearchBox() {
+    return (
+        <Flex
+            as='label'
+            position='relative'
+            flex='1'
+            color='gray.200'
+            bg='gray.800'
+            maxWidth={400}
+            py='4'
+            px='8'
+            ml='6'
+            borderRadius='full'
+            alignSelf='center'
+        >
+            <Input
+                color='gray.50'
+                variant='unstyled'
+                px='4'
+                mr='4'
+                placeholder='Buscar na plataforma'
+                _placeholder={{ color: 'gray.400' }}
+            />
+
+            <Icon as={RiSearchLine} fontSize='20' />
+
+        </Flex>
+    )
+}
