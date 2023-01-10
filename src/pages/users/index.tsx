ee5ab6fb8a7cb8e993bab763d9205d991f7b92c4
Jorge Espindola <jlespindolaf@gmail.com>
2023-01-10 14:24:14 -0300

    Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -113,7 +113,11 @@ export default function UserList() {
                                 </Tbody>
                             </Table>
 
-                            <Pagination />
+                            <Pagination
+                                totalCountOfRegisters={200}
+                                currentPage={19}
+                                onPageChange={() => {}}
+                            />
                         </>
                     )}
                 </Box>
