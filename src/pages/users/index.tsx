aab4b034aa606e82c74cb3ad235f6a7cd10fcf26
Jorge Espindola <jlespindolaf@gmail.com>
2023-01-10 10:13:46 -0300

    Listing api users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,12 +7,32 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+}
+
 export default function UserList() {
     const { data, isLoading, error } = useQuery('users', async () => {
         const response =  await fetch('http://localhost:3000/api/users')
         const data = await response.json()
         
-        return data
+        const users = data.users.map((user: User) => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: "numeric"
+                })
+            }
+        })
+
+        return users
     })
 
     const isWideVersion =  useBreakpointValue({
@@ -70,37 +90,41 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight='bold'>
-                                                    Jorge Espindola
-                                                </Text>
-                                                <Text fontSize='sm' color='gray.300'>
-                                                    jlespindola@gmail.com
-                                                </Text>
-                                            </Box>
-                                        </Td>
-                                        { isWideVersion && (
-                                            <Td>
-                                                12 de dezembro, 2022 
-                                            </Td>
-                                        )}
-                                        <Td>
-                                            <Button
-                                                as='a'
-                                                size='sm'
-                                                fontSize='sm'
-                                                colorScheme='purple'
-                                                leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                                            >
-                                                { isWideVersion ? 'Editar ' : '' }
-                                            </Button>
-                                        </Td>
-                                    </Tr>
+                                    {data.map((user: User) => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight='bold'>
+                                                            {user.name}
+                                                        </Text>
+                                                        <Text fontSize='sm' color='gray.300'>
+                                                            {user.email}
+                                                        </Text>
+                                                    </Box>
+                                                </Td>
+                                                { isWideVersion && (
+                                                    <Td>
+                                                        {user.createdAt}
+                                                    </Td>
+                                                )}
+                                                <Td>
+                                                    <Button
+                                                        as='a'
+                                                        size='sm'
+                                                        fontSize='sm'
+                                                        colorScheme='purple'
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                                                    >
+                                                        { isWideVersion ? 'Editar ' : '' }
+                                                    </Button>
+                                                </Td>
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
 
